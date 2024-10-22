14b1e73e603a23e16318cbc0d316a1a47cca2124
mmccc34 <mmccc34@icloud.com>
2024-10-22 09:38:05 +0200

    avec les points virgule

--- tp03.sql
+++ tp03.sql
@@ -5,15 +5,15 @@ USE compta;
 
 -- Question b. --
 -- SELECT ref, designation FROM article
--- WHERE prix > 2
+-- WHERE prix > 2;
 
 -- Question c. --
 -- SELECT * FROM article
--- WHERE prix>=2 AND prix<=6.25
+-- WHERE prix>=2 AND prix<=6.25;
 
 -- Question d. --
 -- SELECT * FROM article
--- WHERE prix BETWEEN 2 AND 6.25
+-- WHERE prix BETWEEN 2 AND 6.25;
 
 -- Question e. --
 -- SELECT * FROM article
@@ -22,16 +22,16 @@ USE compta;
 
 -- Question f. --
 -- SELECT * FROM article
--- WHERE ID_FOU = 1 || ID_FOU = 3
+-- WHERE ID_FOU = 1 || ID_FOU = 3;
 
 -- Question g. --
 -- SELECT * FROM article
--- WHERE ID_FOU IN (1,3)
+-- WHERE ID_FOU IN (1,3);
 
 -- Question h. --
 -- SELECT * FROM article
--- WHERE ID_FOU NOT IN (1,3)
+-- WHERE ID_FOU NOT IN (1,3);
 
 -- Question i. --
 -- SELECT * FROM bon
--- WHERE Date_CMDE BETWEEN '2019-02-01' AND '2019-04-30'
+-- WHERE Date_CMDE BETWEEN '2019-02-01' AND '2019-04-30';
